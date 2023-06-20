6b973ba80bec9970a9b6c7a6af8bee60f36ce5f0
KiriRainCat <2287990465@qq.com>
2023-06-20 13:38:32 +0800

    ✨ feat: 增加binding required

--- Model/Entity/User.go
+++ Model/Entity/User.go
@@ -2,6 +2,6 @@ package Entity
 
 type User struct {
 	ID       int    `json:"id,omitempty" gorm:"primaryKey"`
-	Username string `json:"username,omitempty"`
-	Password string `json:"password,omitempty"`
+	Username string `json:"username,omitempty" binding:"required"`
+	Password string `json:"password,omitempty" binding:"required"`
 }
